ba1f120a13c54e8d0ef7e1fd646dec461ca59621
Matthew Sklepowich <matthew@sklepowich.com>
2022-07-29 15:59:22 -0700

    fixing user-prompted stars not appearing

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 const nav = document.querySelector('#nav')
 window.addEventListener('scroll', fixNav)
 
-function fixNav(){
-    if(window.scrollY > nav.offsetHeight + 150) {
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
     }
 }
 //Color-changing Button
-function colorChange(){
+function colorChange() {
     var elements = document.getElementsByClassName('colorchange');
     console.log(elements);
-    for (var i = 0; i < elements.length; i++){
+    for (var i = 0; i < elements.length; i++) {
         console.log(elements[i].className.split(' '));
         if (elements[i].className.split(' ').includes('green')) {
             elements[i].className = 'colorchange'
@@ -24,7 +24,7 @@ function colorChange(){
 }
 //Slideshow Timer
 var i = 0;
-var images =[];
+var images = [];
 var time = 5000;
 
 images[0] = 'images/slideshow/finish0.png';
@@ -32,10 +32,10 @@ images[1] = 'images/slideshow/finish1.png';
 images[2] = 'images/slideshow/finish2.png';
 images[3] = 'images/slideshow/finish3.png';
 
-function changeImg(){
+function changeImg() {
     document.slide.src = images[i];
 
-    if(i < images.length - 1){
+    if (i < images.length - 1) {
         i++;
     } else {
         i = 0;
@@ -49,34 +49,34 @@ window.onload = changeImg();
 function getName() {
     let name = prompt("Hi there! Please tell me your name!");
     if (name != "") {
-      document.getElementById("inputName").innerHTML =
-      "- " + name;
+        document.getElementById("inputName").innerHTML =
+            "- " + name;
     } else {
         document.getElementById("inputName").innerHTML =
-        "- The coolest cat around";
+            "- The coolest cat around";
     }
-  }
+}
 
-  window.onload = getName();
-  //User Prompt Number
-  var getNum = prompt("Hey, no reason, but please let me know your favorite number between 1 and 10.");
-  while (true) {
-      if (isNaN(getNum)) {
-          alert("That's not a number, please retry.");
-      } else if (getNum < 0 || getNum > 10) {
-          alert("I asked between 1 and 10, try again.");
-      } else {
+window.onload = getName();
+//User Prompt Number
+var getNum = prompt("Hey, no reason, but please let me know your favorite number between 1 and 10.");
+while (true) {
+    if (isNaN(getNum)) {
+        alert("That's not a number, please try again.");
+    } else if (getNum < 0 || getNum > 10) {
+        alert("I asked between 1 and 10, please try again.");
+    } else {
         document.getElementById("inputNum").innerHTML =
-        getNum + "/" + getNum;
+            getNum + "/" + getNum;
         for (var i = 0; i < getNum; i++) {
             var img = document.createElement("img");
             img.src = "../images/star.png";
             var src = document.getElementById("starBox");
             src.appendChild(img);
         }
-          break;
-      }
-      var getNum = prompt("Please tell me your favorite number between 1 and 10.");
-  }
+        break;
+    }
+    var getNum = prompt("Please tell me your favorite number between 1 and 10.");
+}
 
-window.onload = getNum();  
+window.onload = getNum;
